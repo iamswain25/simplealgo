5f39c46915a1312457308cbf355df51735ec11f2
swain <iamswain@hotmail.com>
2019-10-05 18:59:14 -0700

    rollup

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import typescript from "rollup-plugin-typescript2";
+import pkg from "./package.json";
+import { terser } from "rollup-plugin-terser";
+export default {
+  input: "src/Simplealgo.ts", // our source file
+  output: {
+    file: pkg.main,
+    format: "cjs"
+  },
+  external: [...Object.keys(pkg.dependencies || {})],
+  plugins: [
+    typescript({
+      typescript: require("typescript")
+    }),
+    terser() // minifies generated bundles
+  ]
+};
